bf61b4125dcd775f5c15e7b72a2fc337bcbf2e55
Leandro Souza <leandro.agenciamit@gmail.com>
2023-11-07 10:52:19 -0300

    script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-cp /var/www/html/ .env.example
+cp /var/www/html/ ../.env.example
 mv /var/www/html/.env.example /var/www/html/.env
 composer install --no-dev --working-dir=/var/www/html
 
@@ -15,5 +15,10 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Running NPM..."
+npm install
+npm run build
+
 
 echo "done deploying"
