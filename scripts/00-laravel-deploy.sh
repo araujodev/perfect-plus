133a0522fb401d443bd4aed556bce429c6b5a17a
Leandro Souza <leandro.agenciamit@gmail.com>
2023-11-07 10:48:39 -0300

    mv

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-cp /var/www/html/.env .env.example
+cp /var/www/html/ .env.example
+mv /var/www/html/.env.example /var/www/html/.env
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Clearing caches..."
